007acdf64c95766e63d4c76e606b79a7577821e7
Osmir Mariano <osmirmariano@uft.edu.br>
2017-03-24 16:32:28 -0300

    Criando Fibonacci

--- exercicio4_26.c
+++ exercicio4_26.c
@@ -3,9 +3,41 @@
 #include <pthread.h>
 #include <math.h>
 
-int main(){
-    int valor;
+void fibonacciCalculo(int valor){
+    int anterior = 0, atual = 1;
+    int resultado = 0;
+    int i;
 
-    printf("INFORME O VALOR PARA CALCULAR FIBONACCI: ");
-    scanf("%d", &valor);
+    int vetor[valor];
+    vetor[0] = 0;
+    vetor[1] = 1;
+
+    for(i=2; i<=valor; i++){
+        resultado = atual + anterior;
+        anterior = atual;
+        atual = resultado;
+        vetor[i] = resultado;
+    }
+    //return vetor;
+    // for(int x = 0; x < valor; x++){
+    //     printf("%d\n", vetor[x]);
+    // }
 }
+
+int main(){
+    int valor, recebe;
+    pthread_t tred;
+
+    printf("INFORME NÚMERO DE ELEMENTOS FIBONACCI: ");
+    scanf("%d", &valor);
+
+    recebe = pthread_create(&tred, NULL, fibonacciCalculo(valor), NULL);
+
+    if (recebe){
+         printf("ERROR; retornando o código para pthread_create() is %d\n", recebe);
+         exit(-1);
+    }
+    //pthread_join(tred, NULL);
+    return 0;
+}
+
